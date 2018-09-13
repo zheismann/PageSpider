52fc3f7a0b1e9b2b88d95fdac8ae9505703802f1
zheismann <zac.heismann@gmail.com>
2018-09-13 10:43:33 -0600

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_file_path: str):
+    # TODO implement create_database
+    pass
+
+def save_words_to_database(database_file_path: str, words_list: list):
+    # TODO implement save_words_to_database
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    # TODO implement load_urls_from_file
+    pass
+
+def load_page(url: str):
+    # TODO implement load_page
+    pass
+
+def scrape_page(page_contents: str):
+    # TODO implement scrape_page
+    pass
